d2bb1af4a9777d2d5ead1647d182367c91701399
Pharap <2933055+Pharap@users.noreply.github.com>
2019-04-03 23:53:01 +0100

    Fix pokitto_copy_bin
    Due to an oversight I was using an unevaluated variable.
    Until I can figure out how to evaluate such variables properly,
    I've added a specific workaround.

--- pokitto_copy_bin.py
+++ pokitto_copy_bin.py
@@ -5,12 +5,27 @@ import shutil
 
 # Copy files
 def copy_bin(target, source, env):
-	build_bin = env['BUILD_DIR']
-	project_bin = env['PROJECT_DIR']
+	# os.path.join is a Temporary work around
+	# until I can find a way to evaluate env['BUILD_DIR']
+
+	# Project build directory
+	build_directory = os.path.join(env['PROJECTBUILD_DIR'], env['PIOENV'])
+
+	# Project root directory
+	project_directory = env['PROJECT_DIR']
+
 	for source_file in source:
+		# Get the file name
 		file_name = source_file.name
-		source = os.path.join(build_bin, file_name)
-		shutil.copy(source, project_bin)
+
+		# Find the file in build
+		build_bin = os.path.join(build_directory, file_name)
+
+		# Find the destination in project root
+		project_bin = os.path.join(project_directory, file_name)
+
+		# Copy file
+		shutil.copy(build_bin, project_bin)
 
 # Queue action for after program build
 env.AddPostAction("buildprog", copy_bin)
